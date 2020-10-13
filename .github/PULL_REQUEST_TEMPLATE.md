a2abfac927a8a5017c9eab26c3a184ba5c445022
granddivino <granddivino@gmail.com>
2020-10-13 15:33:13 -0600

    Gabriel G. - Mateen: Fetch-Reddit Deliverable

--- .github/PULL_REQUEST_TEMPLATE.md
+++ .github/PULL_REQUEST_TEMPLATE.md
@@ -2,8 +2,8 @@
 
 Please describe your comfort and completeness levels before submitting.
 
-Comfort Level (1-5): 
+Comfort Level (1-5): 2.5 - Was able to meet requirements, and I tried to do it on my own for the most part, but I had to ask for help and google a lot from DOM manipulation. Although I feel good that it was done, I still can't help but feel like a bit of an imposter since I needed help, and it took me hours over a few days to do this as opposed to some students. Is this normal at this point in time.
 
-Completeness Level (1-5):
+Completeness Level (1-5): 4.5 Didn't have time to figure out how to make a transition between slides. Forgot to do a lot of pseudocode since I was concentrated on getting it to work and just constantly changing things, I'm sorry.
 
-What did you think of this deliverable?:
+What did you think of this deliverable?: It's a good example for what I think is coming so it definitely helps me recognize what I need to brush up on. I'd like to 100% be able to do it on my own at some point.
---
+++ app.js
@@ -0,0 +1,88 @@
+//Declarations
+const requestUrl = "https://www.reddit.com/search.json?q=";
+const input = document.querySelector('#input')
+
+let submission = document.querySelector('#submission')
+let searchbox = document.querySelector('.searchbox')
+let main = document.querySelector('main')
+let body = document.querySelector('body')
+
+let stop = document.createElement('button')
+stop.innerText = 'Stop Slideshow'
+
+//Function to fetch request
+function getPics(request) {
+    let pictures = [];
+    
+    fetch(request)
+
+    .then((responseData)=> {
+        return responseData.json()
+    })
+
+    .then((jsonData)=> {
+        pictures = jsonData.data.children.map(child => child.data.thumbnail).filter(urls => urls[0] === 'h')
+        slideshow(pictures)
+    })
+
+    .catch((error)=> {
+        console.log("Error!", error)
+    })
+
+}
+
+//Function for slideshow of pictures
+function slideshow(pictures) {
+    searchbox.parentNode.removeChild(searchbox)
+    //Calls pitures for the slideshow
+    print(pictures)
+    interval = setInterval(print, 2000, pictures)
+    body.appendChild(stop)
+}
+
+//Function to call pictures
+function print(pictures) {
+    let img = document.createElement('img')
+    let pic = pictures[counter]
+    let removeImage = document.querySelector('img')
+    let counter = 0
+    
+    if(main.firstElementChild) {
+        main.removeChild(removeImage)
+    }
+    
+    img.setAttribute('src', pic)
+    main.appendChild(img)
+    counter ++
+
+    if(counter === pictures.length) {
+        counter = 0
+    }
+}
+
+//Restart function
+function restart() {
+    let interval
+    clearInterval(interval)
+
+    while(main.firstChild) {
+        main.removeChild(main.firstChild)
+    }
+
+    main.appendChild(searchbox)
+    body.removeChild(stop)
+    input.value = ''
+}   
+
+
+document.addEventListener('DOMContentLoaded', ()=> {
+    submission.addEventListener('submit', (event)=> {
+        event.preventDefault()
+        getPics(requestUrl + input.value)
+    })
+
+    stop.addEventListener('click',(event)=>{
+        event.preventDefault()
+        restart()
+    })
+})
